b7932356e8c234cc10b9b30818d62b8f68063d43
David Sutoyo <sutoyod@yahoo.com>
2011-04-17 20:51:58 -0700

    Updating the files for SASS3

--- code/config.rb
+++ code/config.rb
@@ -5,6 +5,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
+line_comments = false;
 output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
