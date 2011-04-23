7b796d8876f15ec87a59e9cecc8fe6d27b2b8b6e
David Sutoyo <sutoyod@yahoo.com>
2011-04-23 15:10:48 -0700

    Add Formalize

--- code/config.rb
+++ code/config.rb
@@ -2,7 +2,7 @@
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 line_comments = false;
